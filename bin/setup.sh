7e3b61d72f162f6a8c83183259bdd5a0de277040
badasswp <badasswpdev@gmail.com>
2026-05-07 01:33:30 +0000

    push to master-built

--- bin/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wp-env run cli wp theme activate twentytwentythree
-wp-env run cli wp rewrite structure /%postname%
-wp-env run cli wp option update blogname "Image Converter for WebP"
-wp-env run cli wp option update blogdescription "Image Converter for WebP"
